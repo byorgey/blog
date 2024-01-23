6f3dd750e46e3e9f269a7d1f9170edca6feea286
Brent Yorgey <byorgey@gmail.com>
2024-01-23 14:44:50 -0600

    work on cleaning script

--- posts/clean.py
+++ posts/clean.py
@@ -4,12 +4,18 @@ import in_place
 for root, dirs, files in os.walk(os.path.curdir):
     for f in files:
         full = os.path.join(root, f)
-        if os.path.splitext(f) in ['.markdown', '.md']:
+        if os.path.splitext(f)[1] in ['.markdown', '.md']:
             with in_place.InPlace(full) as md:
                 meta = False
                 for line in md:
-                    if line == "---":
+                    rewritten = False
+                    if line == "---\n":
                         meta = not meta
                     fields = line.split(':')
                     if meta and fields[0] == 'title' and len(fields) > 1:
-                        fields[1].strip()
+                        md.write(f'{fields[0]}: \'{fields[1].strip()}\'\n')
+                        rewritten = True
+
+                    if not rewritten:
+                        md.write(line)
+
